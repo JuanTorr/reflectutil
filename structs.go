aeeb22e96ea1eb9cd841e427de9eca63fbc77d14
Juan Torres <torrhdzj@gmail.com>
2019-03-10 22:30:53 -0600

    array of struct, test, return error for not implemented types

--- structs.go
+++ structs.go
@@ -4,7 +4,7 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
-	"log"
+
 	"reflect"
 	"time"
 )
@@ -12,34 +12,67 @@ import (
 //ErrInvalidTag defines the error when the tags are invalids
 var ErrInvalidTag = errors.New("Invalid tag")
 
-//TransformStructArr Trnasformforms the struct array from src type to dest type
-func TransformStructArr(dest, src interface{}) {
-	//TODO:
+//TransStructArr Trnasformforms the struct array from struct array  type to dest struct array
+func TransStructArr(dest, src interface{}) error {
+	destv := reflect.ValueOf(dest)
+	if destv.Kind() != reflect.Ptr {
+		return fmt.Errorf("non-pointer %v", destv.Type())
+	}
+	// get the value that the pointer v points to.
+	destv = destv.Elem()
+	if destv.Kind() != reflect.Slice {
+		return fmt.Errorf("can't fill non-slice value")
+	}
+
+	return setSliceValue(destv, reflect.ValueOf(src))
 }
 
-//MarshallTransformStruct realiza el marshall de la estructura transformada
-func MarshallTransformStruct(dest, src interface{}) ([]byte, error) {
+//TransStruct Transforms the src struc to the dest struct
+func TransStruct(dest, src interface{}) error {
+	destv := reflect.ValueOf(dest)
+	if destv.Kind() != reflect.Ptr {
+		return fmt.Errorf("%v not a pointer", destv.Type())
+	}
+	// get the value that the pointer v points to.
+	destv = destv.Elem()
+	if destv.Kind() != reflect.Struct {
+		return fmt.Errorf("can't fill non-struct value")
+	}
+	return transformStruct(destv, reflect.ValueOf(src))
+}
 
+//MarshalTransStructArr Marsharlls and transforms the struct array from struct array  type to dest struct array
+func MarshalTransStructArr(dest, src interface{}) ([]byte, error) {
+	destv := reflect.ValueOf(dest)
+	if destv.Kind() == reflect.Ptr {
+		destv = destv.Elem()
+	} else if destv.Kind() == reflect.Slice {
+		destv = reflect.New(destv.Type()).Elem()
+	} else {
+		return []byte{}, fmt.Errorf("%s not a pointer nor slice", destv.Type())
+	}
+	err := setSliceValue(destv, reflect.ValueOf(src))
+	if err != nil {
+		return []byte{}, err
+	}
+	return json.Marshal(destv.Interface())
+}
+
+//MarshallTransStruct realiza el marshall de la estructura transformada
+func MarshallTransStruct(dest, src interface{}) ([]byte, error) {
+	destv := reflect.ValueOf(dest)
 	switch reflect.TypeOf(dest).Kind() {
 	case reflect.Ptr:
-		TransformStruct(dest, src)
-		return json.Marshal(dest)
 	case reflect.Struct:
-		destv := reflect.New(reflect.TypeOf(dest))
-		transformStruct(destv.Elem(), reflect.ValueOf(src))
-		return json.Marshal(destv.Interface())
+		destv = reflect.New(reflect.TypeOf(dest))
 	default:
-		panic("not pointer or struct")
+		return []byte{}, fmt.Errorf("%s not a struct nor slice", destv.Type())
 	}
-}
-
-//TransformStruct Transforms the src struc to the dest struct
-func TransformStruct(dest, src interface{}) error {
-	return transformStruct(reflect.ValueOf(dest).Elem(), reflect.ValueOf(src))
-}
-
-func transformStructArr(destv, srcv reflect.Value) {
-	//TODO:
+	err := transformStruct(destv.Elem(), reflect.ValueOf(src))
+	if err != nil {
+		return []byte{}, err
+	}
+	return json.Marshal(dest)
 }
 
 func transformStruct(destv, srcv reflect.Value) error {
@@ -49,102 +82,116 @@ func transformStruct(destv, srcv reflect.Value) error {
 		if err != nil {
 			return err
 		}
-		log.Print(" From ", tag.From, " ")
+		//log.Print(" From ", tag.From, " ")
 		v := srcv.FieldByName(tag.From)
-		setValue(destv.Field(i), v)
-		log.Println()
+		err = setValue(destv.Field(i), v)
+		if err != nil {
+			return err
+		}
+		//log.Println()
 	}
 	return nil
 }
 
-func setValue(destv, srcv reflect.Value) {
+func setValue(destv, srcv reflect.Value) error {
 	if !srcv.IsValid() {
-		return
+		return nil
 	}
 	switch destv.Kind() {
 	case reflect.String:
-		setString(destv, srcv)
+		return setString(destv, srcv)
 	case reflect.Struct:
-		setStruct(destv, srcv)
+		return setStruct(destv, srcv)
 	case reflect.Ptr:
-		setPtrValue(destv, srcv)
+		return setPtrValue(destv, srcv)
 	case reflect.Slice:
-		setSliceValue(destv, srcv)
+		return setSliceValue(destv, srcv)
 	}
+	return fmt.Errorf("%s type not implemented", destv.Kind())
 }
 
-func setString(destv, srcv reflect.Value) {
+func setString(destv, srcv reflect.Value) error {
 	switch srcv.Kind() {
 	case reflect.Ptr:
-		setValue(destv, srcv.Elem())
-		return
+		return setValue(destv, srcv.Elem())
 	}
 
 	switch v := srcv.Interface().(type) {
 	case time.Time:
-		log.Printf(" = setting_time[%v] ", srcv)
+		//log.Printf(" = setting_time[%v] ", srcv)
 		destv.SetString(v.Format(time.RFC3339))
-		return
+		return nil
 	}
 
-	log.Printf(" = setting_%v[%v] ", srcv.Kind(), srcv)
+	//log.Printf(" = setting_%v[%v] ", srcv.Kind(), srcv)
 	destv.SetString(fmt.Sprint(srcv))
+	return nil
 }
 
-func setStruct(destv, srcv reflect.Value) {
+func setStruct(destv, srcv reflect.Value) error {
 	switch k := srcv.Kind(); k {
 	case reflect.Struct:
-		transformStruct(destv, srcv)
+		return transformStruct(destv, srcv)
 	case reflect.Ptr:
-		setValue(destv, srcv.Elem())
+		return setValue(destv, srcv.Elem())
 	default:
-		panic(fmt.Sprintf("Can't set kind %v to struct", k))
+		return fmt.Errorf("Can't set kind %v to struct", k)
 	}
 }
 
-func setPtrValue(destv, srcv reflect.Value) {
+func setPtrValue(destv, srcv reflect.Value) error {
 	switch srcv.Kind() {
 	case reflect.Ptr:
-		setValue(destv, srcv.Elem())
-		return
+		return setValue(destv, srcv.Elem())
+
 	}
 	if srcv.IsValid() {
 		ptr := reflect.New(destv.Type().Elem())
-		setValue(ptr.Elem(), srcv)
+		err := setValue(ptr.Elem(), srcv)
+		if err != nil {
+			return err
+		}
 		destv.Set(ptr)
+		return nil
 	}
+	return nil
 }
 
-func setSliceValue(destv, srcv reflect.Value) {
+func setSliceValue(destv, srcv reflect.Value) error {
 	switch destv.Type().Elem().Kind() {
 	case reflect.Struct:
-		setSliceStruct(destv, srcv)
+		return setSliceStruct(destv, srcv)
 	default:
-		setSliceString(destv, srcv)
+		return setSliceString(destv, srcv)
 	}
 }
 
-func setSliceString(destv, srcv reflect.Value) {
-	log.Print("setSliceString ")
+func setSliceString(destv, srcv reflect.Value) error {
+	//log.Print("setSliceString ")
 	l := srcv.Len()
 	if l == 0 {
-		return
+		return nil
 	}
 	s := make([]string, l)
 	for i := 0; i < l; i++ {
 		s[i] = fmt.Sprint(srcv.Index(i))
 	}
 	destv.Set(reflect.ValueOf(s))
+	return nil
 }
-func setSliceStruct(destv, srcv reflect.Value) {
-	log.Print("setSliceStruct ")
+func setSliceStruct(destv, srcv reflect.Value) error {
+	//log.Print("setSliceStruct ")
 	l := srcv.Len()
 	if l == 0 {
-		return
+		return nil
 	}
 	s := reflect.MakeSlice(reflect.SliceOf(destv.Type().Elem()), l, l)
 	for i := 0; i < l; i++ {
-		transformStruct(s.Index(i), srcv.Index(i))
+		err := transformStruct(s.Index(i), srcv.Index(i))
+		if err != nil {
+			return err
+		}
 	}
 	destv.Set(s)
+	return nil
 }
--- structs_test.go
+++ structs_test.go
@@ -2,9 +2,25 @@ package structs
 
 import (
 	"fmt"
+	"math/rand"
 	"testing"
 	"time"
 )
+
+func init() {
+	rand.Seed(time.Now().UTC().UnixNano())
+}
+
+func randString() string {
+	n := rand.Intn(65)
+	const alphanum = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+	var bytes = make([]byte, n)
+	rand.Read(bytes)
+	for i, b := range bytes {
+		bytes[i] = alphanum[b%byte(len(alphanum))]
+	}
+	return string(bytes)
+}
 
 type Src struct {
 	Str       string
@@ -25,16 +41,16 @@ type NestedSrc struct {
 }
 
 type Dest struct {
-	TestString  string `trans:"from:Str"`
-	TestString2 string `trans:"from:Num"`
-	SameName    string
-	NilPtr      string
-	Nested      NestedDest
-	Time        string
-	Ptr         *NestedDest `trans:"from:Nested"`
-	StringArr   []string
-	Recursive   *Dest
-	StructArr   []NestedDest
+	NotStr    string `trans:"from:Str"`
+	NotNum    string `trans:"from:Num"`
+	SameName  string
+	NilPtr    string
+	Nested    NestedDest
+	Time      string
+	Ptr       *NestedDest `trans:"from:Nested"`
+	StringArr []string
+	Recursive *Dest
+	StructArr []NestedDest
 }
 type NestedDest struct {
 	ValA string `trans:"from:A"`
@@ -42,79 +58,178 @@ type NestedDest struct {
 	Same string
 }
 
-func TestTransformStruct(t *testing.T) {
-	/* i := 5
+func getSrc() Src {
+	i := rand.Int()
 	a := Src{
-		Str:       "hello",
-		Num:       &i,
-		SameName:  'a',
-		Nested:    NestedSrc{A: 4, B: true, Same: "hi"},
+		Str:       randString(),
+		SameName:  rune(rand.Int()),
+		Nested:    NestedSrc{A: rand.Int(), B: rand.Intn(2) == 1, Same: randString()},
 		Time:      time.Now(),
-		StringArr: []string{"a", "b", "c"},
-		StructArr: []NestedSrc{NestedSrc{Same: "x"}, NestedSrc{Same: "y"}},
+		StringArr: []string{randString(), randString(), randString()},
+		StructArr: []NestedSrc{NestedSrc{Same: randString()}, NestedSrc{Same: randString()}},
 	}
-	i = 6
-	a.Recursive = &Src{
-		Str:       "world",
-		Num:       &i,
-		SameName:  'b',
-		Nested:    NestedSrc{A: 5, B: true, Same: "ho"},
-		Time:      time.Now(),
-		StringArr: []string{"1", "2", "3"},
+	if rand.Intn(2) == 1 {
+		a.Num = &i
 	}
-	b := Dest{}
+	if rand.Intn(2) == 1 {
+		i = rand.Int()
+		a.Recursive = &Src{
+			Str:       randString(),
+			SameName:  rune(rand.Int()),
+			Nested:    NestedSrc{A: rand.Int(), B: rand.Intn(2) == 1, Same: randString()},
+			Time:      time.Now(),
+			StringArr: []string{randString(), randString(), randString()},
+		}
+		if rand.Intn(2) == 1 {
+			a.Recursive.Num = &i
+		}
+	}
+	i = rand.Intn(2)
+	if i == 0 {
+		i = rand.Intn(5)
+		a.StructArr = make([]NestedSrc, i)
+		for j := 0; j < i; j++ {
+			a.StructArr[j] = NestedSrc{A: rand.Int(), B: rand.Intn(2) == 1, Same: randString()}
+		}
+	}
 
-	err := TransformStruct(&b, a)
-	if err != nil {
-		t.Error(err)
-	} else {
-		fmt.Printf("\n\nRESULT: %+v\n", b)
-		if b.Recursive != nil {
-			fmt.Println(*b.Recursive)
-		}
-		if b.Ptr != nil {
-			fmt.Println(*b.Ptr)
-		}
-		fmt.Println()
-	}
-	*/
+	return a
 }
 
-func TestMarshalStruct(t *testing.T) {
-	type Nested struct {
-		Delta   bool
-		Epsilon float64
+func compareStructValues(dest Dest, src Src) (where string, ok bool) {
+	ok = true
+	compareok := func(currentOK bool, a string) {
+		if !currentOK {
+			where += fmt.Sprint(":", a)
+			ok = false
+		}
 	}
-	a := struct {
-		Alfa  string
-		Beta  int
-		Gamma Nested
-	}{"Hello", 5, Nested{false, 64}}
+	compareok(dest.NotStr == src.Str, `dest.NotStr == src.Str`)
+	compareok(dest.SameName == fmt.Sprint(src.SameName), `dest.SameName == fmt.Sprint(src.SameName)`)
+	compareok(compareNested(dest.Nested, src.Nested), `compareNested(dest.Nested,src.Nested)`)
+	compareok(dest.Time == src.Time.Format(time.RFC3339), `dest.Time == src.Time.Format(time.RFC3339)`)
+	if src.Num == nil {
+		compareok(dest.NotNum == "", `dest.NotNum == ""`)
+	} else {
+		compareok(dest.NotNum == fmt.Sprint(*src.Num), `dest.NotNum == fmt.Sprint(src.Num)`)
+	}
+	if src.NilPtr == nil {
+		compareok(dest.NilPtr == "", `dest.NilPtr == ""`)
+	} else {
+		compareok(dest.NilPtr == fmt.Sprint(*src.NilPtr), `dest.NilPtr == fmt.Sprint(src.NilPtr)`)
+	}
+	if dest.Recursive != nil {
+		if src.Recursive == nil {
+			compareok(false, `src.Recursive == nil`)
+			return
+		}
+		where, curOk := compareStructValues(*dest.Recursive, *src.Recursive)
+		compareok(curOk, "\n"+where)
+	}
+	compareok(compareArrString(dest.StringArr, src.StringArr), `compareArrString(dest.StringArr, src.StringArr)`)
+	if len(dest.StructArr) != len(src.StructArr) {
+		return where + ":" + "len struct Arr", false
+	}
+	for i, v := range dest.StructArr {
+		compareok(compareNested(v, src.StructArr[i]), `compareNested(dest.Nested,src.Nested)`)
+	}
+	return
+}
 
-	j, err := MarshallTransformStruct(struct {
-		A string `trans:"from:Alfa" json:"tagAlfa"`
-		B string `trans:"from:Beta" json:"tagBeta"`
-		C struct {
-			D string `trans:"from:Delta" json:"tagDelta"`
-			E string `trans:"from:Epsilon" json:"tagEpsilon"`
-		} `trans:"from:Gamma" json:"tagGamma"`
-	}{}, a)
+func compareNested(dest NestedDest, src NestedSrc) bool {
+	return dest.ValA == fmt.Sprint(src.A) && dest.ValB == fmt.Sprint(src.B) && dest.Same == src.Same
+}
+
+func compareArrString(a, b []string) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	for i, v := range a {
+		if v != b[i] {
+			return false
+		}
+	}
+	return true
+}
+func TestTransStructArr(t *testing.T) {
+	for i := 0; i < 100; i++ {
+		var d Dest
+		s := getSrc()
+		err := TransStruct(&d, s)
+		if err != nil {
+			t.Error(err)
+			return
+		}
+		where, ok := compareStructValues(d, s)
+		if !ok {
+			t.Errorf("Incorrect transformation:%s\n\n%+v\n%+v", where, s, d)
+			return
+		}
+	}
+}
+func TestTransStruct(t *testing.T) {
+	i := rand.Intn(100) + 100
+	a := make([]Src, i, i)
+	for i := range a {
+		a[i] = getSrc()
+	}
+	var b []Dest
+	TransStructArr(&b, a)
+	if len(b) != len(a) {
+		t.Error("trans struct len not equal", len(b), len(a))
+	}
+	for i, v := range b {
+		where, ok := compareStructValues(v, a[i])
+		if !ok {
+			t.Errorf("Incorrect transformation:%s\n\n%+v\n%+v", where, a, b)
+			return
+		}
+	}
+}
+func TestMarshalTransStructArr(t *testing.T) {
+	i := rand.Intn(100) + 100
+	a := make([]Src, i, i)
+	for i := range a {
+		a[i] = getSrc()
+	}
+	_, err := MarshalTransStructArr([]Dest{}, a)
+
 	if err != nil {
 		t.Error(err)
 		return
 	}
-	fmt.Println(string(j))
-
 }
+func TestMarshalTransStructArrPtr(t *testing.T) {
+	i := rand.Intn(100) + 100
+	a := make([]Src, i, i)
+	for i := range a {
+		a[i] = getSrc()
+	}
+	var b []Dest
+	_, err := MarshalTransStructArr(&b, a)
 
-/* func BenchmarkTransformStructB(b *testing.B) {
-	i := 5
-	a := Src{Str: "hello", Num: &i, SameName: 'a', Nested: NestedSrc{A: 4, B: true, Same: "hi"}, Time: time.Now()}
-	i = 6
-	a.Recursive = &Src{Str: "world", Num: &i, SameName: 'b', Nested: NestedSrc{A: 5, B: true, Same: "ho"}, Time: time.Now()}
-	for i := 0; i < b.N; i++ {
-		b := Dest{}
-		TransformStruct(&b, a)
+	if err != nil {
+		t.Error(err)
+		return
 	}
 }
-*/
+func TestMarshallTransStruct(t *testing.T) {
+	for i := 0; i < 10; i++ {
+		s := getSrc()
+		_, err := MarshallTransStruct(Dest{}, s)
+		if err != nil {
+			t.Error(err)
+			return
+		}
+	}
+}
+func TestMarshallTransStructPtr(t *testing.T) {
+	for i := 0; i < 10; i++ {
+		s := getSrc()
+		_, err := MarshallTransStruct(Dest{}, s)
+		if err != nil {
+			t.Error(err)
+			return
+		}
+	}
+}
